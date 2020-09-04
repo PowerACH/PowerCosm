a65d9c0a4bd71e34eb93f431589615076cc7c809
Rachel Powe <rachelpowe@gmail.com>
2020-09-03 23:29:13 -0400

    edited navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,21 @@
-import React from 'react'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import React from 'react';
+import { Navbar, Nav} from 'react-bootstrap';
+import '../App.css';
 
 export default function NavBar() {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">E-Commerce</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
+                
+                <Nav className="ml-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="/LogIn">LogIn</Nav.Link>
+                <Nav.Link href="#Shop">Shop</Nav.Link>
+                <Nav.Link href="#Cart">Cart</Nav.Link>
                 </Nav>
-                <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                    Dank memes
-                </Nav.Link>
-                </Nav>
+                
             </Navbar.Collapse>
             </Navbar>
     )
