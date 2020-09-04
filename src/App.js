08eb2f1fbd18588d854d710bd2e138582ef6460f
Rachel Powe <rachelpowe@gmail.com>
2020-09-03 23:28:34 -0400

    added router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar';
+import LogIn from './components/LogIn';
+import Register from './components/Register';
+import { BrowserRouter, Switch, Route, } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-    </div>
-  );
+      <Register />
+    
+
+      <BrowserRouter>
+        <Switch>
+              <Route path="/home">
+                <Home />
+              </Route>
+              <Route path="/LogIn">
+                <LogIn />
+              </Route>
+              <Route path="/register">
+                <Register />
+              </Route>
+            </Switch>
+      </BrowserRouter>
+  </div>
+  )
 }
 
 export default App;
