c0b36843729e5642de0753127ce1b56c2fd00b41
Rachel Powe <rachelpowe@gmail.com>
2020-09-07 14:40:04 -0400

    added home route

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,12 +5,12 @@ import '../App.css';
 export default function NavBar() {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href="#home">E-Commerce</Navbar.Brand>
+            <Navbar.Brand href="/"></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 
                 <Nav className="ml-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/LogIn">LogIn</Nav.Link>
                 <Nav.Link href="#Shop">Shop</Nav.Link>
                 <Nav.Link href="#Cart">Cart</Nav.Link>
