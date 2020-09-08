4afe745081b29b74bc8cf09e083c56a753ac7a69
Rachel Powe <rachelpowe@gmail.com>
2020-09-08 16:03:34 -0400

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer class="page-footer font-small indigo">
+
+            <div class="footer-copyright text-center py-3">© 2020 Copyright:
+                <a href="https://github.com/PowerACH"> Rachel Powe</a>
+            </div>
+
+            </footer>
+        )
+    }
