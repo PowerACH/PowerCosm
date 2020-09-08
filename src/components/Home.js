41347bb33eb26d4205a63075695cfb2d404b780f
Rachel Powe <rachelpowe@gmail.com>
2020-09-08 16:03:50 -0400

    rearranged page layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,31 @@
 import React from 'react'
-import { Row,Col,Button } from 'react-bootstrap'
+import { Row,Col,Button,Image } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-// import Products from './Products'
+import Photo from '../assets/head.png'
+import Footer from './Footer'
 
 export default function Home() {
     return (
         <div>
             <Row className = "home-top-row">
-                <Col xs={2} md={8} className = "home-top-row-header">
-                    <p className = "top-header">Discover how</p> 
-                    <p>BEAUTY IS POWERFUL</p>
+                <Col xs={7} className = "home-top-row-header">
+                    <p>DISCOVER HOW</p> 
+                    <p className = "top-header" >Beauty is Powerful</p>
                     <Link to="/shop" >
-                        <Button variant = "outline-secondary" >Shop Products</Button>
+                        <Button variant = "outline-secondary" size="lg">shop products</Button>
                     </Link>
                 </Col>
-                <Col>
-                    <img className = "header-photo" src="https://images.unsplash.com/photo-1527203561188-dae1bc1a417f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=658&q=80" alt="woman"/>
+                <Col xs={5}>
+                    <img className = "header-photo" src= {Photo} alt="woman"/>
                 </Col>
             </Row>
-            <Row>
-                <Col></Col>
-                <Col></Col>
+            <Row className = "home-second-row">
+                <Col>
+                <Image className = "home-image-left" src="https://images.unsplash.com/photo-1515377905703-c4788e51af15?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" rounded fluid/>
+                </Col>
+                {/* <Col></Col> */}
             </Row>
+            <Footer />
         </div>
     )
 }
