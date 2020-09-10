9699fdb608bcc0e7d507c2311ac578b37a867646
Rachel Powe <rachelpowe@gmail.com>
2020-09-10 01:10:07 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import NavBar from './components/NavBar';
 import LogIn from './components/LogIn';
 import Register from './components/Register';
-import Home from './components/Home';
-import Products from "./components/Products";
+import Home from './components/pages/Home';
+import Makeup from "./components/pages/Makeup";
+import Skincare from "./components/pages/Skincare";
+import Cart from "./components/pages/Cart";
+import ProductInfo from './components/pages/ProductInfo';
+import Footer from './components/Footer'
 import { BrowserRouter, Switch, Route, } from 'react-router-dom';
-import ProductInfo from './components/ProductInfo';
 
 function App() {
   return (
@@ -17,9 +20,12 @@ function App() {
               <Route exact path="/" component = {Home} />
               <Route path="/logIn" component = {LogIn} />
               <Route path="/register" component = {Register} />
-              <Route path="/shop" component = {Products} />
-              <Route path= "/products/:prodName" component={ProductInfo} /> 
+              <Route path="/makeup" component = {Makeup} />
+              <Route path="/skincare" component = {Skincare} />
+              <Route path="/Cart" component = {Cart} />
+              <Route path= "/product/:category/:prodName" component={ProductInfo} />
             </Switch>
+      <Footer />
       </BrowserRouter>
   </div>
   )
