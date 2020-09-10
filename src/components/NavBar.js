34deebc722af6801ad2cc34064f52acae1292799
Rachel Powe <rachelpowe@gmail.com>
2020-09-10 01:11:14 -0400

    changed routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,8 +12,9 @@ export default function NavBar() {
                 <Nav className="ml-auto">
                     <Nav.Link href="/">home</Nav.Link>
                     <Nav.Link href="/LogIn">login</Nav.Link>
-                    <Nav.Link href="/Shop">shop</Nav.Link>
-                    <Nav.Link href="#Cart">cart</Nav.Link>
+                    <Nav.Link href="/makeup">makeup</Nav.Link>
+                    <Nav.Link href="/skincare">skincare</Nav.Link>
+                    <Nav.Link href="/Cart">cart</Nav.Link>
                 </Nav>
                 
             </Navbar.Collapse>
