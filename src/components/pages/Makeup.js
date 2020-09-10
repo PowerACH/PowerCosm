33d575332f47ef8db65032c2b4946f44e4bdff62
Rachel Powe <rachelpowe@gmail.com>
2020-09-10 01:06:09 -0400

    added hooks

---
+++ src/components/pages/Makeup.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import '../../App.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom'
+
+
+function Makeup() {
+    
+    const [makeup, setMakeup] = useState ([]);
+
+    useEffect(() => {
+        getMakeupData()
+    }, [])
+
+    const getMakeupData = async () => {
+        const res = await axios.get("http://localhost:8081/makeup");
+        setMakeup(res.data);
+        return res;
+    }
+
+    const products = makeup.map((products, i) => {
+        return (
+        <div key={i}>
+            <li className="products"  >
+            <Link to= {`/product/${products.category}/${products.prodName}`}>
+                <img className = "product-image" src={products.image} alt="product" /> 
+                </Link>
+                <h6 className = "product-name">{products.prodName}</h6>
+                <h6>${products.price}</h6>                               
+            </li>
+        </div>
+        )
+    })
+
+    
+
+    return (
+        <div className="container">
+                {products}
+        </div>
+    )
+}
+
+export default Makeup
