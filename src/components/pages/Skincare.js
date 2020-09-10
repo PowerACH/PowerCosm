767a6ecb2a7c9fab886d23adac6908000a899a62
Rachel Powe <rachelpowe@gmail.com>
2020-09-10 08:33:19 -0400

    fixed mapping, added key

--- src/components/pages/Skincare.js
+++ src/components/pages/Skincare.js
@@ -20,13 +20,13 @@ function Skincare() {
 
     const products = skincare.map((products, i) => {
         return (
-        <div>
-            <li className="products" key={i} >
-            <Link to= {`/${products.category}/${products.prodName}`}>
+        <div key={i}>
+            <li className="products"  >
+            <Link to= {`/product/${products.category}/${products.prodName}`}>
                 <img className = "product-image" src={products.image} alt="product" /> 
                 </Link>
                 <h6 className = "product-name">{products.prodName}</h6>
-                <h7>${products.price}</h7>                               
+                <h6>${products.price}</h6>                               
             </li>
         </div>
         )
