24876c84196ea263e24cacafb6f43dd35335b845
Rachel Powe <rachelpowe@gmail.com>
2020-09-10 08:32:01 -0400

    added to cart page

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -1,8 +1,46 @@
 import React from 'react';
 
-export default function Cart(props) {
-    const prodId = props.match.params.id;
+export default function Cart() {
+    
+    const [data, setData] = useState ([]);
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = async () => {
+        const res = await axios.get("http://localhost:8081/cart");
+        setData(res.data);
+        return res;
+    }
+
+    const deleteData = async () => {
+        const res = await axios.delete("http://localhost:8081/cart/" + prodName);
+        setData(res.data);
+        return res;
+    }
+    const products = data.map((products, i) => {
+        return (
+        <div key={i}>
+            <li className="products"  >
+                <img className = "product-image" src={products.image} alt="product" /> 
+                <h6 className = "product-name">{products.prodName}</h6>
+                <h6>${products.price}</h6>
+                <Button>Delete</Button>                               
+            </li>
+        </div>
+        )
+    })
+
     return (
-        <h1>Cart</h1>
+    <div className="container">
+        <div className = "left">
+            {products}
+        </div>
+        <div className = "right">
+
+        </div>
+    </div>
     )
 }
+
