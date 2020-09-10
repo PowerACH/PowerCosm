b15ad205113713ab29396c7a04e3bdbfd48c71d2
Rachel Powe <rachelpowe@gmail.com>
2020-09-10 08:32:16 -0400

    removed text from home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row,Col,Button,Image } from 'react-bootstrap'
+import { Row,Col,Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Photo from '../../assets/head.png'
 import Skincare from '../../components/pages/Skincare'
@@ -9,7 +9,7 @@ export default function Home() {
         <div>
             <Row className = "home-top-row">
                 <Col xs={7} className = "home-top-row-header">
-                    <p>DISCOVER HOW</p> 
+                    {/* <p>DISCOVER HOW</p>  */}
                     <p className = "top-header" >Beauty is Powerful</p>
                     <Link to="/makeup" >
                         <Button variant = "outline-secondary" size="lg">shop makeup</Button>
@@ -22,9 +22,6 @@ export default function Home() {
             <Row className = "home-second-row">
                 <h2 className = "skin-header">Skincare</h2>
                 <Skincare />
-                {/* <Col>
-                <Image className = "home-image-left" src="https://images.unsplash.com/photo-1515377905703-c4788e51af15?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" rounded fluid/>
-                </Col> */}
             </Row>
             
         </div>
