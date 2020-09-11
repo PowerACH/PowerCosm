0bc5612ba5f29a2640e60d42ff4286477c394cac
Rachel Powe <rachelpowe@gmail.com>
2020-09-11 08:20:06 -0400

    Added container

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import { Row,Col,Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import Photo from '../../assets/head.png'
-import Skincare from '../../components/pages/Skincare'
+import React from 'react';
+import Photo from '../../assets/head.png';
+import Skincare from '../../components/pages/Skincare';
+import { Container,Row,Col,Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
         <div>
             <Row className = "home-top-row">
                 <Col xs={7} className = "home-top-row-header">
-                    {/* <p>DISCOVER HOW</p>  */}
-                    <p className = "top-header" >Beauty is Powerful</p>
+                    <p className = "home-top-header" >Beauty is Powerful</p>
                     <Link to="/makeup" >
                         <Button variant = "outline-secondary" size="lg">shop makeup</Button>
                     </Link>
                 </Col>
                 <Col xs={5}>
-                    <img className = "header-photo" src= {Photo} alt="woman"/>
+                    <img className = "home-header-photo" src= {Photo} alt="woman"/>
                 </Col>
             </Row>
-            <Row className = "home-second-row">
+            <Container fluid className = "home-second-row">
+                <Row className = "skin-header-container" xs = {12}>
                 <h2 className = "skin-header">Skincare</h2>
+                </Row>
                 <Skincare />
-            </Row>
+            </Container>
             
         </div>
     )
