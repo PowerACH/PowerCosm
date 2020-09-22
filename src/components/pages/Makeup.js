494073acbb4da7b2893cfce18d89a42c44cd8255
Rachel Powe <rachelpowe@gmail.com>
2020-09-22 15:03:25 -0400

    changed api

--- src/components/pages/Makeup.js
+++ src/components/pages/Makeup.js
@@ -13,7 +13,7 @@ function Makeup() {
     }, [])
 
     const getMakeupData = async () => {
-        const res = await axios.get("https://powerbeauty.herokuapp.com/makeup");
+        const res = await axios.get("https://powerbeauty.herokuapp.com/api/makeup");
         setMakeup(res.data);
         return res;
     }
